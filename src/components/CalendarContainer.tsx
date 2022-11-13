21a3c1fafb03f288ceded965b13dcc76dc1c4bec
Joaquín Buxó <joaquin.buxo@gmail.com>
2022-11-13 12:15:59 +0100

    adding spinner while fetching

--- src/components/CalendarContainer.tsx
+++ src/components/CalendarContainer.tsx
@@ -4,14 +4,18 @@ import { fetchOffersByCompany } from "../api/fetchFunctions";
 import { Offer } from "../types";
 import { useState } from "react";
 import { useEffect } from "react";
+import Spinner from "./Spinner";
 
 const CalendarContainer = () => {
   const [offers, setOffers] = useState<Offer[]>();
+  const [spinner, setSpinner] = useState(false);
 
   const getOffers = async () => {
     const companyId = 6;
     const numOffersPage = 10;
+    setSpinner(true);
     const offers = await fetchOffersByCompany(companyId, numOffersPage);
+    setSpinner(false);
     setOffers(offers);
   };
 
@@ -23,9 +27,13 @@ const CalendarContainer = () => {
     <div className="calendar">
       Calendar
       <DayContainer />
-      {offers?.map((offer) => {
-        return <p>{offer.id}</p>;
-      })}
+      {spinner ? (
+        <Spinner></Spinner>
+      ) : (
+        offers?.map((offer) => {
+          return <p key={offer.id}>{offer.id}</p>;
+        })
+      )}
     </div>
   );
 };
