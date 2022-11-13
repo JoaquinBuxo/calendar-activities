6e3fc641e1ab1fcca35952611bb0f01a8d7d0e03
Joaquín Buxó <joaquin.buxo@gmail.com>
2022-11-13 12:10:05 +0100

    creating config file

---
+++ src/config.ts
@@ -0,0 +1,22 @@
+const API_URL: string = "https://api.staging.bsport.io/api/v1/";
+const API_KEY: string | undefined = process.env.REACT_APP_API_KEY;
+
+// Offer url with company id and page size filters
+export const offerUrl = (companyId?: number, pageSize?: number) =>
+  `${API_URL}offer?company=${companyId}&page_size=${pageSize}&api_key=${API_KEY}`;
+
+// Get data from activity
+export const activityUrl = (activityId: number) =>
+  `${API_URL}meta-activity/${activityId}?api_key=${API_KEY}`;
+
+// Get data from member
+export const memberUrl = (memberId: number) =>
+  `${API_URL}member/${memberId}?api_key=${API_KEY}`;
+
+// Get data from coach
+export const coachUrl = (coachId: number) =>
+  `${API_URL}coach/${coachId}?api_key=${API_KEY}`;
+
+// Get data from establishment
+export const establishmentUrl = (establishmentId: number) =>
+  `${API_URL}establishment/${establishmentId}?api_key=${API_KEY}`;
